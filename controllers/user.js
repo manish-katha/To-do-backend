5b9f423af51a45c649ed6d91175cb5363ba45162
manish-katha <manish.sh.katha@gmail.com>
2024-07-03 20:02:07 +0530

    rebcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -9,40 +9,37 @@ export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-  const user = await User.findOne({ email }).select("+password +name");
+    const user = await User.findOne({ email }).select("+password +name");
 
-  if (!user) return next(new ErrorHandler("Invalid Email or Password",400))
+    if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
 
-  const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-  if (!isMatch) return next(new ErrorHandler("Invalid id or Password",400))
+    if (!isMatch) return next(new ErrorHandler("Invalid id or Password", 400));
 
-  console.log(user.name);
-  sendcookie(user, res, `Welcome back, ${user.name}`, 200);
-
-    
+    console.log(user.name);
+    sendcookie(user, res, `Welcome back, ${user.name}`, 200);
   } catch (error) {
-    next(error)
-  }};
+    next(error);
+  }
+};
 
 export const register = async (req, res) => {
- try {
-  const { name, email, password } = await req.body;
+  try {
+    const { name, email, password } = await req.body;
 
-  let user = await User.findOne({ email });
+    let user = await User.findOne({ email });
 
-  if (user)
-    return next(new ErrorHandler("User already Exist",400))
+    if (user) return next(new ErrorHandler("User already Exist", 400));
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  user = await User.create({ name, email, password: hashedPassword });
+    user = await User.create({ name, email, password: hashedPassword });
 
-  sendcookie(user, res, "registered succesfully", 201);
-
- } catch (error) {
-  next (error)
- }
+    sendcookie(user, res, "registered succesfully", 201);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getmyProfile = (req, res) => {
@@ -56,7 +53,8 @@ export const getmyProfile = (req, res) => {
 export const logout = (req, res) => {
   res
     .status(200)
-    .cookie("token", "", { expires: new Date(Date.now()), 
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
       sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Develpoment" ? false : true,
     })
