5191833bb276fd0e9d92f70017987e77642d6de1
manish-katha <manish.sh.katha@gmail.com>
2024-07-05 22:18:06 +0530

    some changes

--- utils/feature.js
+++ utils/feature.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const sendcookie = (user, res, message, status) => {
+export const sendcookie = (user, res, message, status=200) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
 
   res
