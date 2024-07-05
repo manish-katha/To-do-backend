a2a98003dffe39e3dcd2f3edc0fd5cd16d8a3ed4
manish-katha <manish.sh.katha@gmail.com>
2024-07-05 22:50:42 +0530

    cors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import userRouter from "./routes/user.js"
+import userRouter from "./routes/user.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import taskRouter from "./routes/task.js";
@@ -8,29 +8,23 @@ import cors from "cors";
 
 export const app = express();
 
-config(
-  {path: "./data/config.env"}
-);
+config({ path: "./data/config.env" });
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials:true,
-  })
-);
-
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/tasks",taskRouter);
-
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET","POST","PUT","DELETE"],
+//     credentials:true,
+//   })
+// );
+app.use(cors());
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tasks", taskRouter);
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.use(errorMiddleware);
-
-
-
