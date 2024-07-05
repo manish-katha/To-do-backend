8170f922a3a191c275de48fff3f3258bf2d2a315
manish-katha <manish.sh.katha@gmail.com>
2024-07-05 22:12:21 +0530

    serverport checking

--- server.js
+++ server.js
@@ -7,5 +7,7 @@ console.log(process.env.PORT);
 console.log(process.env.MONGO_URl);
 
 app.listen(process.env.PORT, () => {
-  console.log("hello");
+  console.log(
+    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
+  );
 });
