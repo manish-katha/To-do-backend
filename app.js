f02ff682c749ed58c21b7610ab8780260b3c1aca
manish-katha <manish.sh.katha@gmail.com>
2024-07-05 23:43:33 +0530

    frontend

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ config({ path: "./data/config.env" });
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["GET","POST","PUT","DELETE"],
-//     credentials:true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin:  [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+
+console.log( process.env.FRONTEND_URL)
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
